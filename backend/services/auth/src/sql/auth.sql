59086a33d857b7d9b323c38801b6da5616390b42
Ayush <Ayushrajranjan10@gmail.com>
2026-03-06 22:09:56 +0530

    Added User-Recuiter auth system

--- backend/services/auth/src/sql/auth.sql
+++ backend/services/auth/src/sql/auth.sql
@@ -1,7 +1,8 @@
-CREATE TABLE auth(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+CREATE TABLE auth (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  role VARCHAR(20) CHECK (role IN ('user','recruiter')) DEFAULT 'user',
+  created_at TIMESTAMPTZ DEFAULT NOW()
 );
