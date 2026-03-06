7922440325f7db2ffa2eda12279d7fd10da7916d
Ayush <Ayushrajranjan10@gmail.com>
2026-03-06 20:40:06 +0530

    Gateway Proxy Server Added

---
+++ backend/gateway/src/config/index.js
@@ -0,0 +1,6 @@
+export const SERVICES = {
+  AUTH: "http://localhost:8080",
+  USER: "http://localhost:8081",
+  JOBS: "http://localhost:8082",
+  APPLICATION: "http://localhost:8083",
+};
---
+++ backend/gateway/src/routes/proxyRoutes.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { SERVICES } from "../config/index.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+import { roleMiddleware } from "../middleware/roleMiddleware.js";
+
+const router = express.Router();
+
+/**
+ * PUBLIC ROUTES
+ * No token required
+ */
+router.use(
+  "/auth",
+  createProxyMiddleware({
+    target: SERVICES.AUTH,
+    changeOrigin: true,
+    pathRewrite: { "": "/api/v1/auth" },
+  })
+);
+
+/**
+ * PROTECTED ROUTES
+ * Token required
+ */
+
+router.use(
+  "/users",
+  verifyToken,
+  createProxyMiddleware({
+    target: SERVICES.USER,
+    changeOrigin: true,
+    pathRewrite: { "": "/api/v1/user" },
+  })
+);
+
+router.use(
+  "/jobs",
+  verifyToken,
+  createProxyMiddleware({
+    target: SERVICES.JOBS,
+    changeOrigin: true,
+    pathRewrite: { "": "/api/v1/jobs" },
+  })
+);
+
+router.use(
+  "/applications",
+  verifyToken,
+  createProxyMiddleware({
+    target: SERVICES.APPLICATION,
+    changeOrigin: true,
+    pathRewrite: { "": "/api/v1/applications" },
+  })
+);
+
+export default router;
